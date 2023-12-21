79ffa37c6945349dcdf45ee8c68ed06cd3548f38
Tobi Adeleke <tobiaadeleke@gmail.com>
2023-12-21 19:17:52 +0000

    remove submodule

---
+++ portfolio/gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `TobiAdeleke`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
